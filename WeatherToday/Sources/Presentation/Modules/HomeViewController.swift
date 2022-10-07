05cc71f45eb76c5ee032d78f98f62764bef05952
Shayokh144 <shayokhbd144@gmail.com>
2022-10-07 15:38:04 +0700

    add initial ui for taking city name input and showing weather data

--- WeatherToday/Sources/Presentation/Modules/HomeViewController.swift
+++ WeatherToday/Sources/Presentation/Modules/HomeViewController.swift
@@ -2,19 +2,96 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    private let cityTextField: UITextField
+    private let temperatureLabel: UILabel
+    private let humidityLabel: UILabel
+    private var landscapeTopConstraint: NSLayoutConstraint = NSLayoutConstraint()
+    private var portraitTopConstraint: NSLayoutConstraint = NSLayoutConstraint()
+
+    init () {
+        cityTextField = UITextField()
+        temperatureLabel = UILabel()
+        humidityLabel = UILabel()
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        landscapeTopConstraint =  cityTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60.0)
+        portraitTopConstraint =  cityTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 260.0)
         view.backgroundColor = .white
 
-        let helloLabel = UILabel()
-        helloLabel.text = "Hello"
-        helloLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityTextField.delegate = self
+        cityTextField.placeholder = "Write city name here"
+        cityTextField.borderStyle = .roundedRect
+        cityTextField.backgroundColor = .black
+        cityTextField.textColor = .white
 
-        view.addSubview(helloLabel)
+        cityTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cityTextField)
 
         NSLayoutConstraint.activate([
-            helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            cityTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cityTextField.widthAnchor.constraint(equalToConstant: 220.0),
+            cityTextField.heightAnchor.constraint(equalToConstant: 40.0)
         ])
+        updateConstraintForCurrentOrientation()
+
+        temperatureLabel.text = "Temperature _ ℃"
+        temperatureLabel.textAlignment = .center
+        temperatureLabel.textColor = .black
+
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(temperatureLabel)
+        NSLayoutConstraint.activate([
+            temperatureLabel.topAnchor.constraint(equalTo: cityTextField.topAnchor, constant: 60.0),
+            temperatureLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            temperatureLabel.widthAnchor.constraint(equalTo: cityTextField.widthAnchor),
+            temperatureLabel.heightAnchor.constraint(equalTo: cityTextField.heightAnchor)
+        ])
+
+        humidityLabel.text = "Humidity _ %"
+        humidityLabel.textAlignment = .center
+        humidityLabel.textColor = .black
+
+        humidityLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(humidityLabel)
+        NSLayoutConstraint.activate([
+            humidityLabel.topAnchor.constraint(equalTo: temperatureLabel.topAnchor, constant: 40.0),
+            humidityLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            humidityLabel.widthAnchor.constraint(equalTo: cityTextField.widthAnchor),
+            humidityLabel.heightAnchor.constraint(equalTo: cityTextField.heightAnchor)
+        ])
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        updateConstraintForCurrentOrientation()
+    }
+
+    private func updateConstraintForCurrentOrientation() {
+        if UIDevice.current.orientation.isLandscape {
+            portraitTopConstraint.isActive = false
+            landscapeTopConstraint.isActive = true
+        } else {
+            landscapeTopConstraint.isActive = false
+            portraitTopConstraint.isActive = true
+        }
+    }
+}
+
+
+extension HomeViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
     }
 }
