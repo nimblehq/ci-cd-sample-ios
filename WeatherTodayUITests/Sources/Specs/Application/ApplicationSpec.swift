36b66549cf7283c8a4220f03cdc5b16db6822926
Shayokh144 <shayokhbd144@gmail.com>
2022-10-21 11:24:44 +0700

    add ui test for app

--- WeatherTodayUITests/Sources/Specs/Application/ApplicationSpec.swift
+++ WeatherTodayUITests/Sources/Specs/Application/ApplicationSpec.swift
@@ -1,4 +1,3 @@
-import Foundation
 import Nimble
 import Quick
 
@@ -7,6 +6,11 @@ final class ApplicationSpec: QuickSpec {
     override func spec() {
 
         var app: XCUIApplication!
+        let defaultTemperatureText = "Temperature _ ℃"
+        let defaultHumidityText = "Humidity _ %"
+        let floatRegex = "[0-9]+.[0-9]+"
+        let temperaturePredicate = NSPredicate(format: "label MATCHES 'Temperature \(floatRegex) ℃'")
+        let humidityPredicate = NSPredicate(format: "label MATCHES 'Humidity \(floatRegex) %'")
 
         describe("a WeatherToday app") {
 
@@ -19,10 +23,49 @@ final class ApplicationSpec: QuickSpec {
                 app.terminate()
             }
 
-            context("when opens") {
+            context("when no city name entered") {
 
-                it("empty tests") {
-                    expect(true) == true
+                it("should show default temperature text") {
+                    expect(app.staticTexts[defaultTemperatureText].exists) == true
+                }
+
+                it("should show default humidity text") {
+                    expect(app.staticTexts[defaultHumidityText].exists) == true
+                }
+            }
+
+            describe("city name entered") {
+
+                context("when invalid city name entered") {
+
+                    beforeEach {
+                        let cityNameTextField = app.textFields["Write city name here"]
+                        cityNameTextField.tap()
+                        cityNameTextField.typeText("A")
+                        app.keyboards.buttons["return"].tap()
+                    }
+
+                    it("should show default temperature text") {
+                        expect(app.staticTexts[defaultTemperatureText].exists) == true
+                    }
+                }
+
+                context("when valid city name entered") {
+
+                    beforeEach {
+                        let cityNameTextField = app.textFields["Write city name here"]
+                        cityNameTextField.tap()
+                        cityNameTextField.typeText("Dhaka")
+                        app.keyboards.buttons["return"].tap()
+                    }
+
+                    it("should not show temperature text with value") {
+                        expect(app.staticTexts.element(matching: temperaturePredicate).waitForExistence(timeout: 4.0)) == true
+                    }
+
+                    it("should show humidity text with value") {
+                        expect(app.staticTexts.element(matching: humidityPredicate).waitForExistence(timeout: 4.0)) == true
+                    }
                 }
             }
         }
